5ce352d92893ca39e1527394152283112b0bede2
phatkiller <phatkiller@gmail.com>
2017-09-05 20:01:17 -0400

    Update Spork Keys

--- src/spork.h
+++ src/spork.h
@@ -95,8 +95,8 @@ private:
 public:
 
     CSporkManager() {
-        strMainPubKey = "04549ac134f694c0243f503e8c8a9a986f5de6610049c40b07816809b0d1d06a21b07be27b9bb555931773f62ba6cf35a25fd52f694d4e1106ccd237a7bb899fdd";
-        strTestPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+        strMainPubKey = "043b2fcaa1149e58bcab5e270fe1ee37690fbe710ad36fabbce89233a6a4842faa6030da06ecda2358066e7dec1045ebcfcba37c8865af23be651ac625c0e008ef";
+        strTestPubKey = "0474055bd4aad01c80de5d168922bf0bc1a3dd929c2d5d80e43c31c60802d81306c84a1d424e9103e6c617c186915f4e4a7ad2231af4a456aabef1c8b9350b68f9";
     }
 
     std::string GetSporkNameByID(int id);
